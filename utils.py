ec6dab4621514c73a6ebb60cc726546a0fa0e906
EricDeng123 <zhdeng1204@163.com>
2022-10-01 16:33:20 +0800

    新增了utils文件，包含多种工具函数

---
+++ utils.py
@@ -0,0 +1,22 @@
+# 获取表格（应该是四个表格信息）
+def get_table_coord():
+    return
+
+# 获取各表格中单元格图标内容
+def get_icons(table):
+    recog()
+    # icons包含序号信息和图标信息(完成识别)
+    icons = dict()
+    return icons
+
+# 整合四个表格中的图表内容
+def overall_icons():
+    return 
+
+# 识别图标
+def recog():
+    return 
+
+# 判定出现连接
+
+# 将一直连接进行可视化
